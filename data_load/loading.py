3771b04a397c11bd0a44ba2ac592c803c0c65880
Darya Vinogradova <dara.vinogradova@yandex.ru>
2019-11-04 16:06:20 +0300

    Load conversations from UDC: tuples

--- data_load/loading.py
+++ data_load/loading.py
@@ -3,7 +3,15 @@ import pandas as pd
 def get_convs(path):
     df = pd.read_csv(path)
     dialog_ids = df["dialogueID"].unique()
-    convs = []
+    starters = {}
+    for i in range(len(df)):
+        if not df["dialogueID"][i] in starters:
+            starters[df["dialogueID"][i]] = df["from"][i]
+    convs = [0] * len(dialog_ids)
+    i = 0
     for dialog_id in dialog_ids:
-        convs.append(df[df["dialogueID"] == dialog_id]["text"].tolist())
+        convs[i] = list(zip(df[df["dialogueID"] == dialog_id]["from"].tolist(),
+                    df[df["dialogueID"] == dialog_id]["text"].tolist()))
+        i += 1
+
     return convs
