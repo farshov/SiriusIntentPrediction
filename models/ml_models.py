407dc2884521a557f66da3fbd278bb03ebd6fc6d
Polly <pdtarantsova@edu.hse.ru>
2019-11-07 14:38:06 +0300

    Multilabel classification fixed

--- models/ml_models.py
+++ models/ml_models.py
@@ -7,11 +7,12 @@ import quality_metrics.quality_metrics as qm
 
 
 class MLModels:
+
     @staticmethod
-    def build_basic_models(x_train, y_train):
+    def fit_models(x_train, y_train):
         """
         training set (from feature_extractor)
-        training set (from feature_extractor)
+        training set (from feature_extractor) list of lists of label codes
 
         return: metrics on different models
         """
@@ -21,20 +22,40 @@ class MLModels:
         return models
 
     @staticmethod
-    def evaluate_basic_models(models,  x_test, y_test):
+    def predict_model(model, x_test, y_test):
         """
 
-        :param models:
+        :param model:
         :param x_test:
         :param y_test:
         :return:
         """
+        y_pred = model.predict(x_test)
+        acc = qm.get_accuracy(y_test, y_pred)
+        f1 = qm.get_f1(y_test, y_pred)
+        return y_pred, acc, f1
+
+    @staticmethod
+    def test_basic_model_multilabel(self, x_train, y_train, x_test, y_test):
+        models = self.build_basic_models(x_train, y_train[:,0])
+        models_x_test = []
         models_acc = []
         models_f1 = []
-        for model in models:
-            y_pred = model.predict(x_test)
-            acc = qm.get_accuracy(y_test, y_pred)
-            f1 = qm.get_f1(y_test, y_pred)
+        for i in range(len(models)):
+            models_x_test.append(x_test)
+
+        for label_idx in y_test.shape[1]:
+            for i, model in enumerate(models):
+                y_pred = model.predict(models_x_test[i], y_test[:, label_idx])
+                models_x_test[i]["label_"+str(label_idx)] = y_pred
+            if label_idx + 1 < y_test.shape[1]:
+                x_train["label_"+str(label_idx + 1)] = y_train[:, label_idx]
+                models = self.build_basic_models(x_train, y_train[:, label_idx + 1])
+
+        for model_pred in models_x_test:
+            predictions = model_pred[:, len(model_pred.columns)-len(y_test.columns)]
+            acc = qm.get_accuracy(y_test, predictions)
+            f1 = qm.get_f1(y_test, predictions)
             models_acc.append(acc)
             models_f1.append(f1)
         return models_acc, models_f1
